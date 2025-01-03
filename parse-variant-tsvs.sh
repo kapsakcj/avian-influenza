ea4c8d774d2d9be19c9b61c5599b731bba84926a
Curtis Kapsak <kapsakcj@gmail.com>
2025-01-03 21:45:52 +0000

    script works now, but still minor kinks to be worked out

--- parse-variant-tsvs.sh
+++ parse-variant-tsvs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Curtis Kapsak, started 2025-01-02
 
+# USAGE:
+# ./parse-variant-tsvs.sh <input_directory> <output_file>
+# example: ./parse-variant-tsvs.sh /path/to/directory/of/variantsTSVs/ /path/to/output.tsv
+
 # requirements:
 # csvkit (https://csvkit.readthedocs.io/en/latest/), install with 'pip install csvkit'
 
@@ -15,6 +19,9 @@
 # 'pb2_variants'
 # 'pa_variants'
 
+# kill script if any command fails or if any variable is not set or if any pipe fails
+set -eou pipefail
+
 # take in first argument as the directory to search for *.tsv files
 # take in second argument as the output file name
 INPUT_DIR=$1
@@ -22,13 +29,14 @@ INPUT_DIR=$1
 # set output filename
 OUTPUT_FILE=$2
 
-# create the output file with the header
+# create the output file with the headers
 echo -e "entity:h5n1_flu_specimen_id\tha_variants\tmp_variants\tna_variants\tns_variants\tnp_variants\tpb1_variants\tpb2_variants\tpa_variants" > $OUTPUT_FILE
 
+# get list of all unique SRR accessions from the input directory of TSV files
+SRR_LIST=$(ls ${INPUT_DIR} | cut -d '_' -f 1 | sort -u)
+
 # loop through all *.tsv files in the input directory
-for file in ${INPUT_DIR}/*_variants.tsv; do
-    # get the SRR accession number from the filename
-    SRR=$(basename $file | cut -d'_' -f1)
+for SRR in ${SRR_LIST}; do
     # set file names for each segment per SRR accession number
     HA_FILE=${SRR}_HA_variants.tsv
     MP_FILE=${SRR}_MP_variants.tsv
@@ -40,19 +48,30 @@ for file in ${INPUT_DIR}/*_variants.tsv; do
     PA_FILE=${SRR}_PA_variants.tsv
 
     # check for TSV files that only have a header row and nothing else, if so echo "No variants for ${SRR}_<segment> segment" and continue to the next file
-    if [ $(wc -l ${INPUT_DIR}/${HA_FILE} | cut -d' ' -f1) -eq 1 ]; then
-        echo "No variants for ${SRR}_HA segment"
+    for SEGMENT in HA MP NA NS NP PB1 PB2 PA; do
+      if [ $(wc -l ${INPUT_DIR}/${SRR}_${SEGMENT}_variants.tsv | cut -d ' ' -f1) -eq 1 ]; then
+        echo "${INPUT_DIR}/${SRR}_${SEGMENT}_variants.tsv does not contain any variants"
         continue
-    else
-        echo "Parsing ${SRR}_HA segment"
-        HA_VARIANT_LIST=$(tail -n +2 ${INPUT_DIR}/${HA_FILE} | cut -f2 | tr '\n' ',')
-    fi
+      else
+        echo "Parsing ${SRR}_${SEGMENT} segment"
+        export ${SEGMENT}_VARIANT_LIST=$(tail -n +2 ${INPUT_DIR}/${SRR}_${SEGMENT}_variants.tsv | awk -F'\t' '{printf "%s%s%s,", $17, $20, $19}' | sed 's/,$//')
+        # tail to remove header, awk to grab columns 17, 20, 19, format to "<column17><column20><column19>", sed to remove trailing comma
+        # HA_VARIANT_LIST=$(tail -n +2 ${INPUT_DIR}/${HA_FILE} | awk -F'\t' '{printf "%s%s%s,", $17, $20, $19}' | sed 's/,$//')
+        # MP_VARIANT_LIST=$(tail -n +2 ${INPUT_DIR}/${MP_FILE} | awk -F'\t' '{printf "%s%s%s,", $17, $20, $19}' | sed 's/,$//')
+        # NA_VARIANT_LIST=$(tail -n +2 ${INPUT_DIR}/${NA_FILE} | awk -F'\t' '{printf "%s%s%s,", $17, $20, $19}' | sed 's/,$//')
+        # NS_VARIANT_LIST=$(tail -n +2 ${INPUT_DIR}/${NS_FILE} | awk -F'\t' '{printf "%s%s%s,", $17, $20, $19}' | sed 's/,$//')
+        # NP_VARIANT_LIST=$(tail -n +2 ${INPUT_DIR}/${NP_FILE} | awk -F'\t' '{printf "%s%s%s,", $17, $20, $19}' | sed 's/,$//')
+        # PB1_VARIANT_LIST=$(tail -n +2 ${INPUT_DIR}/${PB1_FILE} | awk -F'\t' '{printf "%s%s%s,", $17, $20, $19}' | sed 's/,$//')
+        # PB2_VARIANT_LIST=$(tail -n +2 ${INPUT_DIR}/${PB2_FILE} | awk -F'\t' '{printf "%s%s%s,", $17, $20, $19}' | sed 's/,$//')
+        # PA_VARIANT_LIST=$(tail -n +2 ${INPUT_DIR}/${PA_FILE} | awk -F'\t' '{printf "%s%s%s,", $17, $20, $19}' | sed 's/,$//')
+        
+      fi
+    done
+  # now that each segment has been parsed, write to the output file
+  echo "finished parsing each segment variant TSV file, appending $SRR variants to output file..."
+  echo -e "$SRR\t$HA_VARIANT_LIST\t$MP_VARIANT_LIST\t$NA_VARIANT_LIST\t$NS_VARIANT_LIST\t$NP_VARIANT_LIST\t$PB1_VARIANT_LIST\t$PB2_VARIANT_LIST\t$PA_VARIANT_LIST" >> $OUTPUT_FILE
 
-    # for every non-header row in the each variants.tsv file, parse columns 2-4, format to "<column4><column3><column2>" and write it to the output file in a comma-separated list
-    MP=$(tail -n +2 ${INPUT_DIR}/${MP_FILE} | cut -f2 | tr '\n' ',')
-    HA=$(tail -n +2 ${INPUT_DIR}/${HA_FILE} | cut -f2 | tr '\n' ',')
+  echo "done parsing all segments for ${SRR}, continuing to next SRR..."
 
-    
-    # write the variants to the output file
-    echo -e "${SRR}\t${HA}\t${MP}\t${NA}\t${NS}\t${NP}\t${PB1}\t${PB2}\t${PA}" >> $OUTPUT_FILE
 done
+echo "DONE! Output file is located at $OUTPUT_FILE"
